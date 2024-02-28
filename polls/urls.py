5aa8bb3ddb9cae5682b19cb3f2f61a5fc0e6c9fd
Gemark M. Almacen <gmrk08@gmail.com>
2024-02-28 11:30:25 +0800

    another login endpoint using obtain_auth_token method provided by DRF

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 # from .views import polls_list, polls_detail
 from .apiviews import PollList, PollDetail, ChoiceList, CreateVote, UserCreate, LoginView
-from rest_framework.routers import DefaultRouter
 from .apiviews import PollViewSet
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
 
 router = DefaultRouter()
 router.register('polls', PollViewSet, basename='polls')
@@ -17,7 +18,8 @@ urlpatterns = [
     path("polls/<int:pk>/choices/", ChoiceList.as_view(), name="choice_list"),
     path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="create_vote"),
     path("users/", UserCreate.as_view(), name="user_create"),
-    path("login/", LoginView.as_view(), name="login")
+    # path("login/", LoginView.as_view(), name="login")
+    path("login/", views.obtain_auth_token, name="login") # this login endpoint is provided DRF (Django Rest Framework)
 ]
 
 urlpatterns += router.urls
