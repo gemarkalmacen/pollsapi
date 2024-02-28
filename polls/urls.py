eea77c1b1c070424384277cfb23517cfbc029b0f
Gemark M. Almacen <gmrk08@gmail.com>
2024-02-28 11:33:59 +0800

    revert back using asview

--- polls/urls.py
+++ polls/urls.py
@@ -18,8 +18,8 @@ urlpatterns = [
     path("polls/<int:pk>/choices/", ChoiceList.as_view(), name="choice_list"),
     path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="create_vote"),
     path("users/", UserCreate.as_view(), name="user_create"),
-    # path("login/", LoginView.as_view(), name="login")
-    path("login/", views.obtain_auth_token, name="login") # this login endpoint is provided DRF (Django Rest Framework)
+    path("login/", LoginView.as_view(), name="login")
+    # path("login/", views.obtain_auth_token, name="login") # this login endpoint is provided DRF (Django Rest Framework)
 ]
 
 urlpatterns += router.urls
